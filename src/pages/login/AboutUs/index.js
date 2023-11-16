a7b787518a5a28809c75de104a5dde9aa23dcf6e
Luís Augusto Sousa <83593485+D1p7@users.noreply.github.com>
2023-11-16 12:03:04 -0300

    Update index.js

--- src/pages/login/AboutUs/index.js
+++ src/pages/login/AboutUs/index.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 import Doctor from "../../../assets/images/Doctor.png";
 export default function Form() {
   return (
-    <Box display="flex" flexDirection="column" alignItems="center" marginTop={13} sx={{backgroundColor: '#000'}}>
+    <Box display="flex" flexDirection="column" alignItems="center">
       <Image src={Doctor} alt="Doctor" />
       <Typography variant="h4" textAlign={"center"} sx={{marginBottom: 2}}>Sobre nós</Typography>
       <Box>
