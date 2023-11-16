798ea69fd6d35eaa6a4d7830b061b68e6253f693
Luís Augusto Sousa <83593485+D1p7@users.noreply.github.com>
2023-11-16 12:52:58 -0300

    Update index.js

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -104,6 +104,7 @@ if (typeof window !== 'undefined') {
       flexDirection="column"
       justifyContent={"center"}
       bgcolor={"#5e95ff"}
+      paddingTop={13}
     >
       <Navbar handleNav={setPage} />
       <Box
@@ -113,7 +114,6 @@ if (typeof window !== 'undefined') {
         border={0}
         // alignItems={"center"}
         // justifyContent={"center"}
-        marginTop={13}
         bgcolor={"#fff"}
         padding={6}
         gap={5}
