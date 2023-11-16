fbbe531ae610ffdc8928d84fda64dc74708227ff
Luís Augusto Sousa <83593485+D1p7@users.noreply.github.com>
2023-11-16 19:29:36 -0300

    Update AI.js

--- src/pages/api/AI.js
+++ src/pages/api/AI.js
@@ -19,7 +19,7 @@ export default async function handler(req, res) {
     data = [data];
 
     const response = await axios.post(
-      "https://a38a-35-190-167-215.ngrok-free.app/process_data",
+      "https://3245-34-125-199-126.ngrok-free.app/process_data",
       JSON.stringify(data),
       {
         headers: {
